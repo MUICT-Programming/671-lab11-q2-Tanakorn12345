e153861fa24714566b284e5b7b1a4309bf1518c6
Tanakorn12345 <147680957+Tanakorn12345@users.noreply.github.com>
2024-10-19 00:03:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# YOUR CODE HERE
+n = int(input())
+
+l1 = []
+l2 = []
+
+for i in range (n):
+    nn = int(input())
+    l1.append(nn)
+
+for i in range (n):
+    nn = int(input())
+    l2.append(nn)
+
+def  summation(l1,l2):
+    global n
+    up = [0]*n
+    for i in range(n):
+      up[i] = l1[i] + l2[i]
+    return up
+
+def find_min_max(up):
+    return min(up),max(up)
+
+print(summation(l1,l2))
+print(find_min_max(summation(l1,l2)))
